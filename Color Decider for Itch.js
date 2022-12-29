189ad231428e3649f58fec943c266c79a19e2c79
Fenn <nicolaileff@icloud.com>
2022-12-29 08:59:07 -0600

    Add files via upload

---
+++ Color Decider for Itch.js
@@ -0,0 +1,23 @@
+var Count;
+
+function mathRandomInt(a, b) {
+  if (a > b) {
+    // Swap a and b to ensure a is smaller.
+    var c = a;
+    a = b;
+    b = c;
+  }
+  return Math.floor(Math.random() * (b - a + 1) + a);
+}
+
+function colourRandom() {
+  var num = Math.floor(Math.random() * Math.pow(2, 24));
+  return '#' + ('00000' + num.toString(16)).substr(-6);
+}
+
+
+Count = 1;
+while (Count <= mathRandomInt(1, 100)) {
+  window.alert(colourRandom());
+  Count = Count + 1;
+}
