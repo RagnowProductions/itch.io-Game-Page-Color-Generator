418f57352e864151d74feb8eac73aa32b16815ad
Fenn <nicolaileff@icloud.com>
2022-12-29 18:25:09 -0600

    Update Color Decider for Itch.js

--- Color Decider for Itch.js
+++ Color Decider for Itch.js
@@ -2,16 +2,6 @@ function generate() {
 var Count;
 var Color;
 
-function mathRandomInt(a, b) {
-  if (a > b) {
-    // Swap a and b to ensure a is smaller.
-    var c = a;
-    a = b;
-    b = c;
-  }
-  return Math.floor(Math.random() * (b - a + 1) + a);
-}
-
 function colourRandom() {
   var num = Math.floor(Math.random() * Math.pow(2, 24));
   return '#' + ('00000' + num.toString(16)).substr(-6);
@@ -20,7 +10,7 @@ function colourRandom() {
 
 Count = 1;
 Color = colourRandom();
-while (Count <= mathRandomInt(1, 1)) {
+while (Count <= 1) {
   window.alert(color);
   document.getElementById("block").style.color = color;
   Count = Count + 1;
