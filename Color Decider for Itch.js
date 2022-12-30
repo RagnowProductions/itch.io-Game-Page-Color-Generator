1d89af427fa6f3eece8f9d70de2cf8ca67582645
Fenn <nicolaileff@icloud.com>
2022-12-30 08:25:17 -0600

    Update Color Decider for Itch.js

--- Color Decider for Itch.js
+++ Color Decider for Itch.js
@@ -1,16 +1,17 @@
 function generate() {
-var Color;
+  var Color;
 
-function colourRandom() {
-  var num = Math.floor(Math.random() * Math.pow(2, 24));
-  return '#' + ('00000' + num.toString(16)).substr(-6);
-}
+  function colourRandom() {
+    var num = Math.floor(Math.random() * Math.pow(2, 24));
+    return '#' + ('00000' + num.toString(16)).substr(-6);
+  }
 
 
-Color = colourRandom();
+  Color = colourRandom();
 
-window.alert(Color);
-const ucodeblock = document.getElementById("block");
-ucodeblock.style.color = Color;
+  window.alert(Color);
+  const ucodeblock = document.getElementById("block");
+  ucodeblock.style.color = Color;
+  document.getElementById("hexadecimal-paragraph").innerHTML = Color;
 
 }
