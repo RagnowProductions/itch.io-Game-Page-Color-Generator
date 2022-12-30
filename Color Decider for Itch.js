ab96b784db9837c634248010e3544fb4cc681625
Fenn <nicolaileff@icloud.com>
2022-12-30 08:17:24 -0600

    Update Color Decider for Itch.js

--- Color Decider for Itch.js
+++ Color Decider for Itch.js
@@ -12,7 +12,7 @@ Color = colourRandom();
 window.alert(Color);
 const ucodeblock = document.getElementById("block");
 ucodeblock.style.color = Color;
-var copyBobBtn = document.querySelector('.js-copy-bob-btn'),
+var copyBobBtn = document.getElementById('.js-copy-bob-btn'),
 
 copyBobBtn.addEventListener('click', function(event) {
   copyTextToClipboard(Color);
